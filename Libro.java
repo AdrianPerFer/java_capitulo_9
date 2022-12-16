b89ada9b9e03605b84824a8497db84db2aa12903
AdrianPerFer <chopon007@gmail.com>
2022-12-16 18:43:36 +0100

    Only testing

---
+++ Libro.java
@@ -0,0 +1,12 @@
+/**
+ * Libro.java
+ * Definición de la clase Libro
+ * @author Adrián Perogil Fernández
+ */
+public class Libro { // Clase
+    // Atributos
+    String isbn;
+    String titulo;
+    String autor;
+    int numeroDePaginas;
+}
---
+++ PruebaLibro.java
@@ -0,0 +1,13 @@
+/**
+ * PruebaLibro.java
+ * Programa que prueba la clase Libro
+ * @author Adrián Perogil Fernández
+ */
+public class PruebaLibro {
+    public static void main(String[] args) {
+        // Instancias (Objetos)
+        Libro lib = new Libro();
+        Libro miLibrito = new Libro();
+        Libro quijote = new Libro();
+    }
+}
